6c032ed20dfe2261a5f85a3cac0ce3c89388f7ad
Thalaaashu <63043086+Thalaaashu@users.noreply.github.com>
2020-07-15 11:01:40 +0530

    Update ExampleInstrumentedTest.java

--- ExampleInstrumentedTest.java
+++ ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.ashikrabbani.oresturent;
+package com.ashishsingh.oresturent;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ashikrabbani.oresturent", appContext.getPackageName());
+        assertEquals("com.ashishsingh.oresturent", appContext.getPackageName());
     }
 }
