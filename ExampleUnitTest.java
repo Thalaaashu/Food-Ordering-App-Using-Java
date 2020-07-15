f9718f54e2d22145f38243690a8c28ea86affc46
Thalaaashu <63043086+Thalaaashu@users.noreply.github.com>
2020-07-15 11:02:41 +0530

    Update ExampleUnitTest.java

--- ExampleUnitTest.java
+++ ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.ashikrabbani.oresturent;
+package com.ashishsingh.oresturent;
 
 import org.junit.Test;
 
@@ -12,6 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        assertEquals(6, 3 + 3);
     }
 }
